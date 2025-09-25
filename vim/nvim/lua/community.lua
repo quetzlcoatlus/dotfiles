fbf89dbdfb8a8aafec5208fbca73c50f4527340e
quetzlcoatlus <lnalex2456@gmail.com>
2025-09-25 14:59:01 -0700

    updated nvim config

--- vim/nvim/lua/community.lua
+++ vim/nvim/lua/community.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
@@ -9,4 +9,11 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.color.transparent-nvim" },
+  { import = "astrocommunity.pack.typescript-all-in-one" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.prettier" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.game.leetcode-nvim" },
 }
